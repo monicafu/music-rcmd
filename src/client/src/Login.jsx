b81fef2ef068aec32bccecc7894f8aba7ae49c65
Hongbin Li <hblee12294@gmail.com>
2018-03-14 17:14:04 -0700

    Finished Login

--- src/client/src/Login.jsx
+++ src/client/src/Login.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Login = ({userData}) => {
 	return (
 		<div className="login">
-			<div className="user-name">{userData.name}</div>
+			<div className="dropdown">
+				<div className="login-content">
+						<a href="#">Profile</a>
+						<a href="#">Logout</a>
+				</div>
+				<div className="user-name">{userData.name}</div>
+			</div>
 		</div>
 	);
 }
